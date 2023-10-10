b9dcadc0742290daa4d4b8f5954202de99bdb649
anastasiaangelo <145339311+anastasiaangelo@users.noreply.github.com>
2023-10-10 14:34:22 +0200

    corrected until line 90

--- model_proteincsv2.py
+++ model_proteincsv2.py
@@ -1,16 +1,14 @@
 import pyrosetta; pyrosetta.init()
-
 from pyrosetta.teaching import *
 from pyrosetta import *
 
-import csv
-from pyrosetta.rosetta.core.scoring import EMapVector
-from pyrosetta.rosetta.core.pack.rotamer_set import RotamerSet, RotamerSets
-from pyrosetta.rosetta.core.pack.rotamer_set import *
+from pyrosetta.rosetta.core.pack.rotamer_set import RotamerSets, RotamerSet
 from pyrosetta.rosetta.core.pack.task import TaskFactory
 from pyrosetta.rosetta.core.pack.rotamer_set import *
+from pyrosetta.rosetta.core.pack.interaction_graph import InteractionGraphFactory
 from pyrosetta.rosetta.core.pack.task import *
 
+import csv
 import sys
 import numpy as np
 
@@ -26,7 +24,6 @@ def has_hydrogen_atoms(pose):
             if pose.residue(i).atom_name(j)[0] == 'H':
                 return True
     return False
-
 
 #Check if the pose has hydrogen atoms
 if has_hydrogen_atoms(pose):
@@ -62,29 +59,43 @@ task_pack = TaskFactory.create_packer_task(pose) # is a class in PyRosetta that 
             # A packer task is a task that defines which residues in a protein structure (pose) are allowed to move or be optimized during a packing step of a computational protein design or refinement protocol.
 rotsets = RotamerSets()
 
-pose.update_residue_neighbors()
-sfxn.setup_for_packing(pose, task_pack.designing_residues(), task_pack.designing_residues())
+#Set up and calculate pairwise interaction energies between rotamers of two specific residues (1 and 2) 
+pose.update_residue_neighbors() # updates the neighbour information for residues in the proetein structure
+sfxn.setup_for_packing(pose, task_pack.designing_residues(), task_pack.designing_residues()) # prepares the scoring function for packing by specifying which residues are allowed to be designed (optimized) based on the task pack, designing_residues() returns the list of residues that are designated as designable in the task pack.
+# packer_task = pyrosetta.rosetta.core.pack.task.PackerTask(pose.total_residue())
+# packer_graph = pyrosetta.rosetta.core.pack.creat_packer_graph(pose, packer_task)
+packer_neighbor_graph = pyrosetta.rosetta.core.pack.create_packer_graph(pose, sfxn, task_pack) #The neighbor graph represents the spatial relationships between residues and is essential for efficient energy calculations.
+rotsets.set_task(task_pack) #associates the rotamer sets with the task pack, indicating which residues and rotamers will be considered during the analysis.
+rotsets.build_rotamers(pose, sfxn, packer_neighbor_graph) #builds rotamers for the protein's residues. Rotamers are alternative conformations of side chains that are sampled during protein modeling.
+rotsets.prepare_sets_for_packing(pose, sfxn) #prepares the rotamer sets for packing calculations by potentially reducing the set of rotamers based on their energies.
+ig = InteractionGraphFactory.create_interaction_graph(task_pack, rotsets, pose, sfxn, packer_neighbor_graph)
+print("built", rotsets.nrotamers(), "rotamers at", rotsets.nmoltenres(), "positions.")
+rotsets.compute_energies(pose, sfxn, packer_neighbor_graph, ig, 1) #This computes energies associated with the rotamers in the context of the protein structure. 
 
+
+#Loop that calculates the pairwise interaction energies between different rotamers (s_i and s_j) at positions 1 and 2 in the protein. The energies are stored in the E 
 
 #Analyse energy between residues
 #to isolate the contribution from particular pairs of residues
-# emap = EMapVector()
-n_rots_I = rotsets.nrotamers_for_moltenres(1) # calculating the number of rotamers for residue 1
-n_rots_J = rotsets.nrotamers_for_moltenres(2)
-E = np.zeros((n_rots_I, n_rots_J))
 
+max_rotamers = 0
+for residue_number in range(1, residue_count):
+    n_rots = rotsets.nrotamers_for_moltenres(residue_number)
+    if n_rots > max_rotamers:
+        max_rotamers = n_rots
+
+E = np.zeros((max_rotamers, max_rotamers))
 output_file = "score_summary.csv"
 
 
-
 with open(output_file, "w") as f:
-    for residue_i in range(1, residue_count + 1):
+    for residue_i in range(1, residue_count):
         rotamer_set_i = RotamerSet(pose, residue_i)
 
         for rotamer_i in range(rotamer_set_i.num_rotamers()):
             rotamer_set_i.set_rotamer(rotamer_i)
 
-            for residue_j in range(1, residue_count + 1):
+            for residue_j in range(1, residue_count):
                 if residue_i != residue_j:
                     rotamer_set_j = RotamerSet(residue_j)
 
@@ -97,11 +108,7 @@ with open(output_file, "w") as f:
                         E[rotamer_i, rotamer_j] = interaction_energy
                     
         print("Interaction energy between rotamers of residue 1 and 2:", E[rotamer_i, rotamer_j])
-        # print(emap)
-        # print(emap[fa_atr]) 
-        # print(emap[fa_rep]) 
-        # print(emap[fa_sol])
-        # f.write(f"Score Interactions between residue {residue_number} : {residue1.name3()} and residue {residue_number+1} : {residue2.name3()} --->> Vdw attractive term: {emap[fa_atr]:.2f} Vdw repulsive term: {emap[fa_rep]:.2f} Solvation term: {emap[fa_sol]:.2f} \n\n\n")
+        f.write(f"Score Interactions between rotamer {rotamer_i} and rotamer {rotamer_j} --->> {E[rotamer_i,rotamer_j]}")
 
 
 

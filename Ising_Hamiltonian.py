fc8cf724fc6b99a6551a00088912f3fe1c300991
anastasiaangelo <145339311+anastasiaangelo@users.noreply.github.com>
2023-10-11 11:24:35 +0200

    hamiltonian Jij values

--- Ising_Hamiltonian.py
+++ Ising_Hamiltonian.py
@@ -14,7 +14,7 @@ from pyrosetta.rosetta.core.pack.rotamer_set import *
 from pyrosetta.rosetta.core.pack.interaction_graph import InteractionGraphFactory
 from pyrosetta.rosetta.core.pack.task import *
 
-#initiate structure, scorefunction
+#Initiate structure, scorefunction
 pose = pyrosetta.pose_from_pdb("inputs/6Q21_A.pdb")
 residue_count = pose.total_residue()
 sfxn = get_score_function(True)
@@ -23,7 +23,7 @@ relax_protocol = pyrosetta.rosetta.protocols.relax.FastRelax()
 relax_protocol.set_scorefxn(sfxn)
 relax_protocol.apply(pose)
 
-#define task and interaction graph
+#Define task and interaction graph
 task_pack = TaskFactory.create_packer_task(pose) 
 rotsets = RotamerSets()
 pose.update_residue_neighbors()
@@ -36,7 +36,7 @@ ig = InteractionGraphFactory.create_interaction_graph(task_pack, rotsets, pose, 
 print("built", rotsets.nrotamers(), "rotamers at", rotsets.nmoltenres(), "positions.")
 rotsets.compute_energies(pose, sfxn, packer_neighbor_graph, ig, 1)
 
-#define dimension for matrix
+#Define dimension for matrix
 max_rotamers = 0
 for residue_number in range(1, residue_count):
     n_rots = rotsets.nrotamers_for_moltenres(residue_number)
@@ -64,23 +64,62 @@ def spin_down():
 #     row.append('new value')
 
 
+#Loop to find hamiltonian values Jij
 with open(output_file, "w") as f:
     for residue_number in range(1, residue_count + 1):
         residue1 = pose.residue(residue_number)
-        S1 = spin_up()
-        if residue_number == residue_count:
-            break
-        residue2 = pose.residue(residue_number + 1)
-        S2 = spin_down()
-        sfxn.eval_ci_2b(residue1, residue2, pose, emap)
-        print("Interaction score values of", residue1.name3(), "and", residue2.name3())
-        f.write(f"Score Interaction between residue number {residue_number} : {residue1.name3()} and {residue_number+1} : {residue2.name3()} --> Vdw attractive term: {emap[fa_atr]:.2f} Vdw repulsive term: {emap[fa_rep]:.2f} Solvation term: {emap[fa_sol]:.2f} \n\n")
-        Hamiltonian[residue_number, residue_number+1] = emap[fa_atr]*S1*S2
-        emap.zero()
+        rotamer_set_i = rotsets.rotamer_set_for_residue(residue_number)
+        if rotamer_set_i == None: # skip if no rotamers for the residue
+            continue
 
+        for residue_number2 in range(1, residue_count+1):
+            residue2 = pose.residue(residue_number + 1)
+            rotamer_set_j = rotsets.rotamer_set_for_residue(residue_number2)
+            if rotamer_set_j == None:
+                continue
 
+        molten_res_i = rotsets.resid_2_moltenres(residue_number)
+        molten_res_j = rotsets.resid_2_moltenres(residue_number2)
 
+        edge_exists = ig.find_edge(molten_res_i, molten_res_j)
+            
+        if not edge_exists:
+                continue
+        
+        for rot_i in range(1, rotamer_set_i.num_rotamers() + 1):
+            for rot_j in range(1, rotamer_set_j.num_rotamers() + 1):
+                S1 = spin_up()
+                S2 = spin_down()
+                E[rot_i-1, rot_j-1] = ig.get_two_body_energy_for_edge(molten_res_i, molten_res_j, rot_i, rot_j)
+                Hamiltonian[rot_i-1, rot_j-1] = E[rot_i-1, rot_j-1]*S1*S2
+    
+        for rot_i in range(1, rotamer_set_i.num_rotamers() + 1):
+            for rot_j in range(1, rotamer_set_j.num_rotamers() + 1):
+                print(f"Interaction energy between rotamers of residue {residue_number} rotamer {rot_i} and residue {residue_number2} rotamer {rot_j} :", E[rot_i-1, rot_j-1])
+                f.write(f"Score Interactions between residue {residue_number} rotamer {rot_i} and residue {residue_number2} rotamer {rot_j} -> {E[rot_i-1, rot_j-1]}\n")
+
+                    
+
+#Loop to find hamiltonian values Jii
 with open(output_file, "a", newline='') as f:
+    for residue_number in range(1, residue_count + 1):
+        residue1 = pose.residue(residue_number)
+        rotamer_set_i = rotsets.rotamer_set_for_residue(residue_number)
+        if rotamer_set_i == None: # skip if no rotamers for the residue
+            continue
+
+        molten_res_i = rotsets.resid_2_moltenres(residue_number)
+        edge_exists = ig.find_edge(molten_res_i, molten_res_j)
+            
+        if not edge_exists:
+                continue
+        
+        for rot_i in range(1, rotamer_set_i.num_rotamers() + 1):
+            S1 = spin_up()
+            E[rot_i-1, rot_i-1] = ig.get_two_body_energy_for_edge(molten_res_i, molten_res_i, rot_i, rot_i)
+            Hamiltonian[rot_i-1, rot_j-1] = E[rot_i, rot_i]*S1
+
+
     for residue_number in range(1, residue_count + 1):
         residue1 = pose.residue(residue_number)
         S1 = spin_up()

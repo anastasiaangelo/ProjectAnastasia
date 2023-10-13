397db51248c8c7e74bcced6ff3edd98397c20cd9
anastasiaangelo <145339311+anastasiaangelo@users.noreply.github.com>
2023-10-13 18:03:13 +0200

    correct libraries

--- optimization_hamiltonian.py
+++ optimization_hamiltonian.py
@@ -1,21 +1,21 @@
 # Script to optimise the Hamiltonian, starting with the QUBO problem, transforming to Ising problem, optimising
-from qiskit.utils import algorithm_globals
-from qiskit.algorithms.minimum_eigensolvers import QAOA, NumPyMinimumEigensolver
-from qiskit.algorithms.optimizers import COBYLA
-from qiskit.primitives import Sampler
-from qiskit_optimization.algorithms import (
-    MinimumEigenOptimizer,
-    RecursiveMinimumEigenOptimizer,
-    SolutionSample,
-    OptimizationResultStatus,
-)
+from qiskit import Aer
+from qiskit.algorithms import QAOA
 from qiskit_optimization import QuadraticProgram
-from qiskit.visualization import plot_histogram
-from typing import List, Tuple
-import numpy as np
-import matplotlib.pyplot as plt
+from qiskit_optimization.algorithms import MinimumEigenOptimizer
 
 ## Converting a QUBO to an operator
-# create a QUBO
+# create a QUBO with two qubits for example
 qubo = QuadraticProgram()
-qubo.binary_var("x")
+qubo.binary_var("x0")
+qubo.binary_var("x1")
+qubo.minimize(linear=[1, 1], quadratic={('x0', 'x1'): 2})
+
+#Create a QAOA based on the quadratic program which approximates the ground state of the Ising Hamiltonian
+# using here a simulator, this can be replaced with a quantum device if we want
+qaoa = MinimumEigenOptimizer(QAOA(quantum_instance=Aer.get_backend('qasm_simulator')))
+result = qaoa.solve(qubo)
+
+#Analyse the result to get the optimal solution for the Ising problem
+print('Optimal solution:', result.x)
+print('Optimal value:', result.fval)

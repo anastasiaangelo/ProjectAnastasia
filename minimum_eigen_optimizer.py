a060d3f55429edf6a60a19325342fa851e791e26
anastasiaangelo <145339311+anastasiaangelo@users.noreply.github.com>
2023-10-13 14:42:53 +0200

    to plot

--- minimum_eigen_optimizer.py
+++ minimum_eigen_optimizer.py
@@ -13,6 +13,7 @@ from qiskit_optimization import QuadraticProgram
 from qiskit.visualization import plot_histogram
 from typing import List, Tuple
 import numpy as np
+import matplotlib.pyplot as plt
 
 ## Converting a QUBO to an operator
 # create a QUBO
@@ -23,7 +24,7 @@ qubo.binary_var("z")
 qubo.minimize(linear=[1, -2, 3], quadratic={("x", "y"): 1, ("x", "z"): -1, ("y", "z"): 2})
 print(qubo.prettyprint())
 
-#Next we translate this QUBO into an Ising operator. This results not only in an Operator but also in a constant offset to 
+#Next we translate this QUBO into an Ising operator. This results in an Operator and also a constant offset to 
 # be taken into account to shift the resulting value.
 op, offset = qubo.to_ising()
 print("offset: {}".format(offset))
@@ -41,14 +42,14 @@ print(qp.prettyprint())
 
 
 ## Solving a QUBO with the MinimumEigenOptimizer
-#  initializing the MinimumEigensolver we want to use.
-algorithm_globals.random_seed = 10598
-qaoa_mes = QAOA(sampler=Sampler(), optimizer=COBYLA(), initial_point=[0.0, 0.0])
-exact_mes = NumPyMinimumEigensolver()
+# initializing the MinimumEigensolver we want to use.
+algorithm_globals.random_seed = 10598       #sets the random seed for the random number generator used by the quantum algorithms in Qiskit. Setting a random seed ensures that the algorithm's behavior is reproducible
+qaoa_mes = QAOA(sampler=Sampler(), optimizer=COBYLA(), initial_point=[0.0, 0.0])    #creates a Quantum Approximate Optimization Algorithm (QAOA) instance
+exact_mes = NumPyMinimumEigensolver()       #creates an instance of the NumPyMinimumEigensolver, a classical algorithm  for solving for the minimum eigenvalue and eigenvector of a matrix
 
 # we use the MinimumEigensolver to create MinimumEigenOptimizer
-qaoa = MinimumEigenOptimizer(qaoa_mes)  # using QAOA
-exact = MinimumEigenOptimizer(exact_mes)  # using the exact classical numpy minimum eigen solver
+qaoa = MinimumEigenOptimizer(qaoa_mes)  # using QAOA, creating an instance of the class
+exact = MinimumEigenOptimizer(exact_mes)  # using the exact classical numpy minimum eigen solver, creating an instance of the class
 
 #We first use the MinimumEigenOptimizer based on the classical exact NumPyMinimumEigensolver to get the optimal benchmark solution for this small example.
 exact_result = exact.solve(qubo)
@@ -58,3 +59,44 @@ print(exact_result.prettyprint())
 qaoa_result = qaoa.solve(qubo)
 print(qaoa_result.prettyprint())
 
+## Analysis of samples
+#Multiple samples corresponding to the same input are consolidated into a single SolutionSample
+print("variable order:", [var.name for var in qaoa_result.variables])
+for s in qaoa_result.samples:
+    print(s)
+
+#you may want to filter samples according to their status or probabilities
+def get_filtered_samples(
+    samples: List[SolutionSample],
+    threshold: float = 0,
+    allowed_status: Tuple[OptimizationResultStatus] = (OptimizationResultStatus.SUCCESS,),
+):
+    res = []
+    for s in samples:
+        if s.status in allowed_status and s.probability > threshold:
+            res.append(s)
+
+    return res
+
+filtered_samples = get_filtered_samples(
+    qaoa_result.samples, threshold=0.005, allowed_status=(OptimizationResultStatus.SUCCESS,)
+)
+for s in filtered_samples:
+    print(s)
+
+
+# to obtain a better insight of the samples -> statistics
+fvals = [s.fval for s in qaoa_result.samples]
+probabilities = [s.probability for s in qaoa_result.samples]
+np.mean(fvals)
+np.std(fvals)
+
+#visualisation
+samples_for_plot = {
+    " ".join(f"{qaoa_result.variables[i].name}={int(v)}" for i, v in enumerate(s.x)): s.probability
+    for s in filtered_samples
+}
+samples_for_plot
+
+plot_histogram(samples_for_plot)
+plt.show()

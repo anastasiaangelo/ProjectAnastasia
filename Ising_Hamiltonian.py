0f1c96855590f0c8359f5d65636c74003c4eae19
anastasiaangelo <145339311+anastasiaangelo@users.noreply.github.com>
2023-10-16 15:48:11 +0200

    pandas output file

--- Ising_Hamiltonian.py
+++ Ising_Hamiltonian.py
@@ -6,6 +6,7 @@ from pyrosetta import *
 import csv
 import sys
 import numpy as np
+import pandas as pd
 
 from matplotlib import pyplot as plt
 from pyrosetta.rosetta.core.pack.rotamer_set import RotamerSets
@@ -48,6 +49,9 @@ E = np.zeros((max_rotamers, max_rotamers))
 Hamiltonian = np.zeros((max_rotamers, max_rotamers))
 
 output_file = "hamiltonian_terms.csv"
+data_list = []
+df = pd.DataFrame(columns=['res i', 'res j', 'rot A_i', 'rot B_j', 'E_ij'])
+
 
 #Spin functions to define Pauli states
 def spin_up():
@@ -58,59 +62,63 @@ def spin_down():
 
 
 #Loop to find Hamiltonian values Jij
-with open(output_file, "w") as f:
-    for residue_number in range(1, residue_count):
-        rotamer_set_i = rotsets.rotamer_set_for_residue(residue_number)
-        if rotamer_set_i == None: # skip if no rotamers for the residue
-            continue
+for residue_number in range(1, residue_count):
+    rotamer_set_i = rotsets.rotamer_set_for_residue(residue_number)
+    if rotamer_set_i == None: # skip if no rotamers for the residue
+        continue
 
-        residue_number2 = residue_number + 1
-        residue2 = pose.residue(residue_number2)
-        rotamer_set_j = rotsets.rotamer_set_for_residue(residue_number2)
-        if rotamer_set_j == None:
-            continue
+    residue_number2 = residue_number + 1
+    residue2 = pose.residue(residue_number2)
+    rotamer_set_j = rotsets.rotamer_set_for_residue(residue_number2)
+    if rotamer_set_j == None:
+        continue
 
-        molten_res_i = rotsets.resid_2_moltenres(residue_number)
-        molten_res_j = rotsets.resid_2_moltenres(residue_number2)
+    molten_res_i = rotsets.resid_2_moltenres(residue_number)
+    molten_res_j = rotsets.resid_2_moltenres(residue_number2)
 
-        edge_exists = ig.find_edge(molten_res_i, molten_res_j)
-            
-        if not edge_exists:
-                continue
+    edge_exists = ig.find_edge(molten_res_i, molten_res_j)
         
-        for rot_i in range(1, rotamer_set_i.num_rotamers() + 1):
-            for rot_j in range(1, rotamer_set_j.num_rotamers() + 1):
-                # S1 = spin_up()
-                # S2 = spin_down()
-                E[rot_i-1, rot_j-1] = ig.get_two_body_energy_for_edge(molten_res_i, molten_res_j, rot_i, rot_j)
-                Hamiltonian[rot_i-1, rot_j-1] = E[rot_i-1, rot_j-1]  #*S1*S2
+    if not edge_exists:
+            continue
     
-        for rot_i in range(1, rotamer_set_i.num_rotamers() + 1):
-            for rot_j in range(1, rotamer_set_j.num_rotamers() + 1):
-                print(f"Interaction energy between rotamers of residue {residue_number} rotamer {rot_i} and residue {residue_number2} rotamer {rot_j} :", Hamiltonian[rot_i-1, rot_j-1])
-                f.write(f"Score Interactions between residue {residue_number} rotamer {rot_i} and residue {residue_number2} rotamer {rot_j} -> {Hamiltonian[rot_i-1, rot_j-1]}\n")
+    for rot_i in range(1, rotamer_set_i.num_rotamers() + 1):
+        for rot_j in range(1, rotamer_set_j.num_rotamers() + 1):
+            # S1 = spin_up()
+            # S2 = spin_down()
+            E[rot_i-1, rot_j-1] = ig.get_two_body_energy_for_edge(molten_res_i, molten_res_j, rot_i, rot_j)
+            Hamiltonian[rot_i-1, rot_j-1] = E[rot_i-1, rot_j-1]  #*S1*S2
 
-                    
+    for rot_i in range(1, rotamer_set_i.num_rotamers() + 1):
+        for rot_j in range(1, rotamer_set_j.num_rotamers() + 1):
+            print(f"Interaction energy between rotamers of residue {residue_number} rotamer {rot_i} and residue {residue_number2} rotamer {rot_j} :", Hamiltonian[rot_i-1, rot_j-1])
+            # f.write(f"Score Interactions between residue {residue_number} rotamer {rot_i} and residue {residue_number2} rotamer {rot_j} -> {Hamiltonian[rot_i-1, rot_j-1]}\n")
+            data = {'res i': residue_number, 'res j': residue_number2, 'rot A_i': rot_i, 'rot B_j': rot_j, 'E_ij': Hamiltonian[rot_i-1, rot_j-1]}
+            data_list.append(data)
+                
 
 #Loop to find Hamiltonian values Jii
-with open(output_file, "a", newline='') as f:
-    for residue_number in range(1, residue_count + 1):
-        residue1 = pose.residue(residue_number)
-        rotamer_set_i = rotsets.rotamer_set_for_residue(residue_number)
-        if rotamer_set_i == None: 
-            continue
+for residue_number in range(1, residue_count + 1):
+    residue1 = pose.residue(residue_number)
+    rotamer_set_i = rotsets.rotamer_set_for_residue(residue_number)
+    if rotamer_set_i == None: 
+        continue
 
-        molten_res_i = rotsets.resid_2_moltenres(residue_number)
-        
-        for rot_i in range(1, rotamer_set_i.num_rotamers() + 1):
-            # S1 = spin_up()
-            E[rot_i-1, rot_i-1] = ig.get_one_body_energy_for_node_state(molten_res_i, rot_i)
-            Hamiltonian[rot_i-1, rot_i-1] = E[rot_i-1, rot_i-1]  #*S1
+    molten_res_i = rotsets.resid_2_moltenres(residue_number)
+    
+    for rot_i in range(1, rotamer_set_i.num_rotamers() + 1):
+        # S1 = spin_up()
+        E[rot_i-1, rot_i-1] = ig.get_one_body_energy_for_node_state(molten_res_i, rot_i)
+        Hamiltonian[rot_i-1, rot_i-1] = E[rot_i-1, rot_i-1]  #*S1
 
-            print(f"Interaction score values of {residue1.name3()} rotamer {rot_i} with itself {Hamiltonian[rot_i-1,rot_i-1]}")
-            f.write(f"Score Interaction of residue {residue_number} : {residue1.name3()}, rotamer {rot_i} with itself --> {Hamiltonian[rot_i-1, rot_i-1]} \n\n")
-        
+        print(f"Interaction score values of {residue1.name3()} rotamer {rot_i} with itself {Hamiltonian[rot_i-1,rot_i-1]}")
+        # f.write(f"Score Interaction of residue {residue_number} : {residue1.name3()}, rotamer {rot_i} with itself --> {Hamiltonian[rot_i-1, rot_i-1]} \n\n")
+        data = {'res i': residue_number, 'res j': residue_number, 'rot A_i': rot_i, 'rot B_j': rot_i, 'E_ij': Hamiltonian[rot_i-1, rot_i-1]}
+        data_list.append(data)
+    
 
 
 #Save the Hamiltonian to a csv file
 np.savetxt("hamiltonian.csv", Hamiltonian, delimiter=",", fmt="%d")
+
+df = pd.DataFrame(data_list)
+df.to_csv('hamiltonian_terms.csv', index=False)

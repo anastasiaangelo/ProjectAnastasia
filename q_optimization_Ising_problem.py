7e40b1d677dba18f1900f37067daef6ab1347a72
anastasiaangelo <145339311+anastasiaangelo@users.noreply.github.com>
2023-10-16 16:32:49 +0200

    pass csv as input

--- q_optimization_Ising_problem.py
+++ q_optimization_Ising_problem.py
@@ -1,13 +1,28 @@
 # Script to optimise the Hamiltonian, starting directly from the Ising Hamiltonian
 import numpy as np
+import csv
 
 ## here we will use the classical code to calculate and import the matrix J
 # use sparse representation for memory reasons
-J = np.array([[1,2], [3,4]])
+with open('hamiltonian_terms.csv', mode='r') as file:
+    csv_reader = csv.reader(file)
+    data = []
+    for row in csv_reader:
+        complex_row = []
+        for entry in row:
+            try:
+                complex_row.append(complex(entry))
+            except ValueError:
+              print(f"Error converting string {entry} to complex. Please check the CSV.")
+              complex_row.append(0j)  # add a default value; or you can skip this row/entry depending on your needs
+        data.append(complex_row)
+    J = np.array(data)  
+
+
 # or build the Pauli representation from the problem may be more efficient rather than converting it
 
 from qiskit import Aer, QuantumCircuit, transpile
-from qiskit.opflow import PauliSumOp, MatrixOp, Z, I
+from qiskit.opflow import PauliSumOp, MatrixOp
 from qiskit.algorithms.minimum_eigensolvers import QAOA
 from qiskit.quantum_info.operators import Operator
 from qiskit.algorithms.optimizers import COBYLA
@@ -21,12 +36,10 @@ hamiltonian = PauliSumOp.from_list(pauli_terms)
 
 print(hamiltonian)
 
-H = -1.0 * (Z ^ Z ^ I ^ I) - 1.0 * (I ^ Z ^ Z ^ I) - 1.0 * (I ^ I ^ Z ^ Z)
-
 
 ## find minimum value using optimisation technique of QAOA
 nqubits = 4
 
-qaoa = QAOA(1,optimizer=COBYLA(), reps=1, mixer=H, initial_point=[1.0,1.0])
-result = qaoa.compute_minimum_eigenvalue(H)
+qaoa = QAOA(1,optimizer=COBYLA(), reps=1, mixer=hamiltonian, initial_point=[1.0,1.0])
+result = qaoa.compute_minimum_eigenvalue(hamiltonian)
 print(result)

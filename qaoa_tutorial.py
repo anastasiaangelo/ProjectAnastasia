a0765f0409bc1d9a196816af023d8051aa8f7a57
anastasiaangelo <145339311+anastasiaangelo@users.noreply.github.com>
2023-10-17 10:01:48 +0200

    print cirucits

--- qaoa_tutorial.py
+++ qaoa_tutorial.py
@@ -7,3 +7,31 @@ G.add_nodes_from([0, 1, 2, 3])
 G.add_edges_from([(0, 1), (1, 2), (2, 3), (3, 0)])
 nx.draw(G, with_labels=True, alpha=0.8, node_size=500)
 plt.show()
+
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from qiskit import Aer, execute
+from qiskit.circuit import Parameter
+
+# Adjacency is essentially a matrix which tells you which nodes are
+# connected. This matrix is given as a sparse matrix, so we need to
+# convert it to a dense matrix
+adjacency = nx.adjacency_matrix(G).todense()
+
+nqubits = 4
+
+beta = Parameter("$\\beta$")
+qc_mix = QuantumCircuit(nqubits)
+for i in range(0, nqubits):
+    qc_mix.rx(2*beta, i)
+
+qc_mix.draw()
+print(qc_mix)
+
+gamma = Parameter("$\\gamma$")
+qc_p = QuantumCircuit(nqubits)
+for pair in list(G.edges()): #pairs of ndoes
+    qc_p.rzz(2*gamma, pair[0], pair[1])
+    qc_p.barrier()
+
+qc_p.decompose().draw()
+print(qc_p)

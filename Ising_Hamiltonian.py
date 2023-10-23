17a71fc4ac693d444ebeb43dd0a2783c524f8058
anastasiaangelo <145339311+anastasiaangelo@users.noreply.github.com>
2023-10-23 15:09:05 +0200

    comment max energy

--- Ising_Hamiltonian.py
+++ Ising_Hamiltonian.py
@@ -1,5 +1,4 @@
 ### Now contrsuct the Hamiltonian for our problem using the interaction energies previously calculated
-# %%
 import pyrosetta;
 pyrosetta.init()
 from pyrosetta.teaching import *
@@ -17,6 +16,7 @@ from pyrosetta.rosetta.core.pack.rotamer_set import *
 from pyrosetta.rosetta.core.pack.interaction_graph import InteractionGraphFactory
 from pyrosetta.rosetta.core.pack.task import *
 from pyrosetta.rosetta.core.pack.task.operation import OperateOnResidueSubset, PreventRepacking
+from pyrosetta import PyMOLMover
 
 #Initiate structure, scorefunction
 pose = pyrosetta.pose_from_pdb("protonated.pdb")
@@ -110,8 +110,9 @@ for residue_number in range(1, residue_count):
             # S1 = spin_up()
             # S2 = spin_down()
             E[rot_i-1, rot_j-1] = ig.get_two_body_energy_for_edge(molten_res_i, molten_res_j, rot_i, rot_j)
-            if E[rot_i-1, rot_j-1] > 3:
-                continue
+            # energy cutoff, leave for now
+            # if E[rot_i-1, rot_j-1] > 3:
+            #     continue
             Hamiltonian[rot_i-1, rot_j-1] = E[rot_i-1, rot_j-1]  #*S1*S2
 
     for rot_i in range(1, rotamer_set_i.num_rotamers() + 1):
@@ -123,7 +124,7 @@ for residue_number in range(1, residue_count):
 
 #Save the two-body energies to a csv file
 df = pd.DataFrame(data_list)
-#all vlaues
+# all vlaues
 # df.to_csv('two_body_terms.csv', index=False)
 # to choose the two rotamers with the largest energy in absolute value
 df.assign(abs_E=df['E_ij'].abs()).nlargest(2, 'abs_E').drop(columns=['abs_E']).to_csv('two_body_terms.csv', index=False)
@@ -141,8 +142,8 @@ for residue_number in range(1, residue_count + 1):
     for rot_i in range(1, rotamer_set_i.num_rotamers() + 1):
         # S1 = spin_up()
         E1[rot_i-1, rot_i-1] = ig.get_one_body_energy_for_node_state(molten_res_i, rot_i)
-        if E[rot_i-1, rot_i-1] > 5:
-            continue
+        # if E[rot_i-1, rot_i-1] > 5:
+        #     continue
         Hamiltonian1[rot_i-1, rot_i-1] = E1[rot_i-1, rot_i-1]  #*S1
 
         # print(f"Interaction score values of {residue1.name3()} rotamer {rot_i} with itself {Hamiltonian[rot_i-1,rot_i-1]}")
@@ -153,7 +154,7 @@ for residue_number in range(1, residue_count + 1):
 
 #Save the one-body energies to a csv file
 df1 = pd.DataFrame(data_list1)
-#all vlaues
+# all vlaues
 # df1.to_csv('one_body_terms.csv', index=False)
 # to choose the two rotamers with the largest energy in absolute value
 df1.assign(abs_Ei=df1['E_ii'].abs()).nlargest(2, 'abs_Ei').drop(columns=['abs_Ei']).to_csv('one_body_terms.csv', index=False)

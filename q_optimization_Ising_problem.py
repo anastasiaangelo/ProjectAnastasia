97128d9d57729fbafdb3239daa83babf29367437
anastasiaangelo <145339311+anastasiaangelo@users.noreply.github.com>
2023-10-30 13:40:00 +0100

    qaoa works

--- q_optimization_Ising_problem.py
+++ q_optimization_Ising_problem.py
@@ -11,14 +11,13 @@ h = df1['E_ii'].values
 h = np.multiply(0.5,h)       #to convert from QUBO to Ising hamiltonian
 num = len(h)
 
-print(h)
+print(f"\nOne body energy values: \n", h)
 
 df = pd.read_csv("two_body_terms.csv")
 value = df['E_ij'].values
 value = np.multiply(0.5, value)
 J = np.zeros((num,num))
 n = 0
-print(value)
 
 for i in range(0, num-2):
     if i%2 == 0:
@@ -30,14 +29,15 @@ for i in range(0, num-2):
         J[i][i+2]=0.5*value[n+1]
         n += 2
 
-print(J)
+print(f"\nTwo body energy values: \n", J)
 
 for i in range(0, num):
     J[i][i] = h[i]
 
-print(J)
+print(f"\nMatrix of all pairwise interaction energies: \n", J)
 
-## function to construct the ising hamiltonain from the one-body and two-body energies h and J
+## Classical optimisation:
+# function to construct the ising hamiltonain from the one-body and two-body energies h and J
 def ising_hamiltonian(config, one_body_energies, two_body_energies):
     hamiltonian = 0
 
@@ -49,7 +49,6 @@ def ising_hamiltonian(config, one_body_energies, two_body_energies):
             hamiltonian +=two_body_energies[i][j] * config[i]*config[j]
         
     return hamiltonian
-
 
 # define a random initial configuration
 initial_config = np.random.choice([-1, 1], size=num)
@@ -67,47 +66,28 @@ result = minimize(energy_function, initial_config, method='COBYLA')
 ground_state_config = result.x
 ground_state_energy = result.fun
 
+print("\n\nClassical optimisation results. \n")
 print("Ground state energy: ", ground_state_energy)
 print("Ground state wavefunction: ", ground_state_config)
 
 
 
-## Find minimum value using optimisation technique of QAOA
-from qiskit import Aer, QuantumCircuit, transpile
-from qiskit.opflow import PauliSumOp, MatrixOp, I, X, Z, PauliOp
+## Quantum optimisation
+#  Find minimum value using optimisation technique of QAOA
+from qiskit import Aer, QuantumCircuit
 from qiskit_algorithms.minimum_eigensolvers import QAOA
-from qiskit.quantum_info.operators import Operator, Pauli
-from qiskit.algorithms.optimizers import COBYLA
-from qiskit.opflow import PauliSumOp
+from qiskit.quantum_info.operators import Operator, Pauli, SparsePauliOp
+from qiskit_algorithms.optimizers import COBYLA
+from qiskit.primitives import Sampler
 
 num_qubits = 4
 
-def Z_op(qubit, num_qubits):
-    """Return a Z Pauli operator on the specified qubit in a num-qubit system."""
-    op_list = [I for _ in range(num_qubits)]
-    op_list[qubit] = Z
-    op = op_list[0]
-    for current_op in op_list[1:]:
-        op = op @ current_op
-    return op
-
-def X_op(qubit, num_qubits):
+def X_op(i, num_qubits):
     """Return an X Pauli operator on the specified qubit in a num-qubit system."""
-    op_list = [I for _ in range(num_qubits)]
-    op_list[qubit] = X
-    op = op_list[0]
-    for current_op in op_list[1:]:
-        op = op @ current_op
-    return op
+    op_list = ['I'] * num_qubits
+    op_list[i] = 'X'
+    return SparsePauliOp(Pauli(''.join(op_list)))
 
-def ising_to_pauli_op(h, J):
-    num = len(h)
-    op = sum(h[i] * Z_op(i, num_qubits) for i in range(num_qubits))
-    for i in range(num_qubits):
-        for j in range(i+1, num_qubits):
-            op += J[i][j] * Z_op(i, num_qubits) @ Z_op(j, num_qubits)
-    return op
- 
 
 def generate_pauli_zij(n, i, j):
     if i<0 or i >= n or j<0 or j>=n:
@@ -121,20 +101,29 @@ def generate_pauli_zij(n, i, j):
 
 hamiltonian_terms = []
 
-for i in range(0, num):
-    for j in range(0, num):
+for i in range(0, num_qubits):
+    for j in range(0, num_qubits):
         if J[i][j] != 0:
-            op = PauliOp(generate_pauli_zij(2**num_qubits, i, j), coeff=J[i][j])
+            pauli = generate_pauli_zij(num_qubits, i, j)
+            op = SparsePauliOp(pauli, coeffs=[J[i][j]])
             hamiltonian_terms.append(op) 
 
-hamiltonian = sum(hamiltonian_terms)
+hamiltonian = sum(hamiltonian_terms, SparsePauliOp(Pauli('I'*num_qubits)))
 
-print(hamiltonian)
+def format_sparsepauliop(op):
+    terms = []
+    labels = [pauli.to_label() for pauli in op.paulis]
+    coeffs = op.coeffs
+    for label, coeff in zip(labels, coeffs):
+        terms.append(f"{coeff:.10f} * {label}")
+    return '\n'.join(terms)
+
+print(f"\nThe hamiltonian constructed using Pauli operators is: \n", format_sparsepauliop(hamiltonian))
 
 
 #the mixer in QAOA should be a quantum operator representing transitions between configurations
 mixer_op = sum(X_op(i,num_qubits) for i in range(num_qubits))
 
-qaoa = QAOA(1, optimizer=COBYLA(), reps=1, mixer=mixer_op, initial_point=[1.0,1.0])
+qaoa = QAOA(sampler=Sampler(), optimizer=COBYLA(), reps=2, mixer=mixer_op, initial_point=[1.0,1.0,1.0,1.0])
 result = qaoa.compute_minimum_eigenvalue(hamiltonian)
-print("\n\nthe result is", result)
+print("\n\nThe result of the quantum optimisation using QAOA is: \n", result)

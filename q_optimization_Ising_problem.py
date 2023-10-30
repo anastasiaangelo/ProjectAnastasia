db6f5582fc20a896764efc908a4d04117ad2b028
anastasiaangelo <145339311+anastasiaangelo@users.noreply.github.com>
2023-10-30 11:44:08 +0100

    ham lin combination of pauli vectors

--- q_optimization_Ising_problem.py
+++ q_optimization_Ising_problem.py
@@ -15,6 +15,7 @@ print(h)
 
 df = pd.read_csv("two_body_terms.csv")
 value = df['E_ij'].values
+value = np.multiply(0.5, value)
 J = np.zeros((num,num))
 n = 0
 print(value)
@@ -31,7 +32,10 @@ for i in range(0, num-2):
 
 print(J)
 
-num_qubits = 4
+for i in range(0, num):
+    J[i][i] = h[i]
+
+print(J)
 
 ## function to construct the ising hamiltonain from the one-body and two-body energies h and J
 def ising_hamiltonian(config, one_body_energies, two_body_energies):
@@ -54,7 +58,6 @@ initial_config = np.random.choice([-1, 1], size=num)
 def energy_function(config):
     return ising_hamiltonian(config, h, J)   
 
-
 ## First we will diagonalise classically and then compare the result with the qaoa
 # Define the optimization function to minimize the Ising Hamiltonian
 from scipy.optimize import minimize
@@ -68,13 +71,16 @@ print("Ground state energy: ", ground_state_energy)
 print("Ground state wavefunction: ", ground_state_config)
 
 
+
 ## Find minimum value using optimisation technique of QAOA
 from qiskit import Aer, QuantumCircuit, transpile
-from qiskit.opflow import PauliSumOp, MatrixOp, I, X, Z
+from qiskit.opflow import PauliSumOp, MatrixOp, I, X, Z, PauliOp
 from qiskit_algorithms.minimum_eigensolvers import QAOA
-from qiskit.quantum_info.operators import Operator
+from qiskit.quantum_info.operators import Operator, Pauli
 from qiskit.algorithms.optimizers import COBYLA
 from qiskit.opflow import PauliSumOp
+
+num_qubits = 4
 
 def Z_op(qubit, num_qubits):
     """Return a Z Pauli operator on the specified qubit in a num-qubit system."""
@@ -101,14 +107,34 @@ def ising_to_pauli_op(h, J):
         for j in range(i+1, num_qubits):
             op += J[i][j] * Z_op(i, num_qubits) @ Z_op(j, num_qubits)
     return op
+ 
 
-hamiltonian_op = ising_to_pauli_op(h, J)
+def generate_pauli_zij(n, i, j):
+    if i<0 or i >= n or j<0 or j>=n:
+        raise ValueError(f"Indices out of bounds for n={n} qubits. ")
+   
+    pauli_str = ['I']*n
+    pauli_str[i] = 'Z'
+    pauli_str[j] = 'Z'
+
+    return Pauli(''.join(pauli_str))
+
+hamiltonian_terms = []
+
+for i in range(0, num):
+    for j in range(0, num):
+        if J[i][j] != 0:
+            op = PauliOp(generate_pauli_zij(2**num_qubits, i, j), coeff=J[i][j])
+            hamiltonian_terms.append(op) 
+
+hamiltonian = sum(hamiltonian_terms)
+
+print(hamiltonian)
+
 
 #the mixer in QAOA should be a quantum operator representing transitions between configurations
 mixer_op = sum(X_op(i,num_qubits) for i in range(num_qubits))
 
-hamiltonian = ising_hamiltonian(initial_config, h, J)  
-
 qaoa = QAOA(1, optimizer=COBYLA(), reps=1, mixer=mixer_op, initial_point=[1.0,1.0])
-result = qaoa.compute_minimum_eigenvalue(hamiltonian_op)
+result = qaoa.compute_minimum_eigenvalue(hamiltonian)
 print("\n\nthe result is", result)

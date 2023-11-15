652efff7e7c8b38d16198f9ced6ab91c5ba777cd
anastasiaangelo <145339311+anastasiaangelo@users.noreply.github.com>
2023-11-15 16:44:47 +0100

    correct classical

--- q_optimization_Ising_problem.py
+++ q_optimization_Ising_problem.py
@@ -46,7 +46,6 @@ for i in range(num):
 
 print('\nH: \n', H)
 
-
 # add penalty terms to the matrix so as to discourage the selection of two rotamers on the same residue - implementation of the Hammings constraint
 def add_penalty_term(M, penalty_constant, residue_pairs):
     for i, j in residue_pairs:
@@ -55,16 +54,66 @@ def add_penalty_term(M, penalty_constant, residue_pairs):
     return M
 
 P = 3
-residue_pairs = [(0,1), (2,3)]       #, (4,5), (6,7)]     #, (8,9), (10,11), (12,13)]
+pairs = [(0,1), (2,3)]       #, (4,5), (6,7)]     #, (8,9), (10,11), (12,13)]
 
 M = deepcopy(H)
-M = add_penalty_term(M, P, residue_pairs)
+M = add_penalty_term(M, P, pairs)       #matrix for qaoa
 
 ## Classical optimisation:
 from scipy.sparse.linalg import eigsh
+num_qubits = num
 
-eigenvalues, eigenvectors = eigsh(M, k=num, which='SA')
+Z_matrix = np.array([[1, 0], [0, -1]])
+identity = np.eye(2)
 
+def construct_operator(qubit_indices, num_qubits):
+    operator = np.eye(1)
+    for qubit in range(num_qubits):
+        if qubit in qubit_indices:
+            operator = np.kron(operator, Z_matrix)
+        else:
+            operator = np.kron(operator, identity)
+    return operator
+
+C = np.zeros((2**num_qubits, 2**num_qubits))
+
+for i in range(num_qubits):
+    operator = construct_operator([i], num_qubits)
+    C += H[i][i] * operator
+
+for i in range(num_qubits):
+    for j in range(i+1, num_qubits):
+        operator = construct_operator([i, j], num_qubits)
+        C += H[i][j] * operator
+
+print('C :\n', C)
+
+def create_hamiltonian(pairs, P, num_qubits):
+    H_pen = np.zeros((2**num_qubits, 2**num_qubits))
+    def tensor_term(term_indices):
+        term = [Z_matrix if i in term_indices else identity for i in range(num_qubits)]
+        result = term[0]
+        for t in term[1:]:
+            result = np.kron(result, t)
+        return result
+    
+    for pair in pairs:
+        term = tensor_term(pair)
+        H_pen += P * term
+
+    return H_pen
+
+# ZZII = np.kron(np.kron(np.kron(Z_matrix, Z_matrix), identity), identity)
+# IIZZ = np.kron(np.kron(np.kron(identity, identity), Z_matrix), Z_matrix)
+# H_penalty = P * (ZZII + IIZZ)
+
+H_penalty = create_hamiltonian(pairs, P, num_qubits)
+
+H_tot = C + H_penalty
+
+# Extract the ground state energy and wavefunction
+# using sparse representation so as to be able to generalise to larger systems
+eigenvalues, eigenvectors = eigsh(H_tot, k=num, which='SA')
 print("\n\nClassical optimisation results. \n")
 print("Ground energy eigsh: ", eigenvalues[0])
 print("ground state wavefuncion eigsh: ", eigenvectors[:,0])
@@ -77,8 +126,6 @@ from qiskit_algorithms.minimum_eigensolvers import QAOA
 from qiskit.quantum_info.operators import Operator, Pauli, SparsePauliOp
 from qiskit_algorithms.optimizers import COBYLA
 from qiskit.primitives import Sampler
-
-num_qubits = num
 
 def X_op(i, num_qubits):
     """Return an X Pauli operator on the specified qubit in a num-qubit system."""
@@ -130,14 +177,13 @@ print(f"\nThe hamiltonian constructed using Pauli operators is: \n", format_spar
 #the mixer in QAOA should be a quantum operator representing transitions between configurations
 mixer_op = sum(X_op(i,num_qubits) for i in range(num_qubits))
 
-
 p = 10  # Number of QAOA layers
 initial_point = np.ones(2 * p)
 qaoa = QAOA(sampler=Sampler(), optimizer=COBYLA(), reps=p, mixer=mixer_op, initial_point=initial_point)
 result = qaoa.compute_minimum_eigenvalue(q_hamiltonian)
 print("\n\nThe result of the quantum optimisation using QAOA is: \n")
 print('best measurement', result.best_measurement)
-print('eigenvalue: ', result.eigenvalue.real)
+print(result)
 
 k = 0
 
@@ -149,9 +195,9 @@ for i in range(num_qubits):
         if i != j:
             k += 0.5 * 0.25 * Q[i][j]
 
-print('\nk: ', k)
+print('eigenvalue: ', np.real(result.best_measurement['value']) + 2*P + k)
 
-# ground state energy calculation with Ising model
+# alternative ground state energy calculation with Ising model
 bitstring = result.best_measurement['bitstring']
 spins = [1 if bit == '0' else -1 for bit in bitstring]
 
@@ -163,8 +209,6 @@ for i in range(num_qubits):
 
 for i in range(num_qubits):
     energy +=  H[i][i] * spins[i]
-
-print(energy)
 
 print(f"The energy for bitstring {bitstring} with J is: {energy + k}")
 
@@ -183,3 +227,5 @@ for i in range(num_qubits):
                 en += 0.5 * Q[i][j] * bits[i] * bits[j]
 
 print(f"The energy for bitstring {bitstring} with Q is: {en}")
+
+print('g.s energy: ', eigenvalues[0]+2*P+k)

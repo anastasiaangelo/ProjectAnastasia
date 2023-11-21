2cfe3fc429c116dee5db7d1e858c40f1f0bfced7
anastasiaangelo <145339311+anastasiaangelo@users.noreply.github.com>
2023-11-21 11:30:31 +0100

    with sparse pauli

--- q_optimization_Ising_problem.py
+++ q_optimization_Ising_problem.py
@@ -185,8 +185,7 @@ qaoa = QAOA(sampler=Sampler(), optimizer=COBYLA(), reps=p, mixer=mixer_op, initi
 result = qaoa.compute_minimum_eigenvalue(q_hamiltonian)
 print("\n\nThe result of the quantum optimisation using QAOA is: \n")
 print('best measurement', result.best_measurement)
-print(result)
-
+# print(result)
 
 k = 0
 for i in range(num_qubits):

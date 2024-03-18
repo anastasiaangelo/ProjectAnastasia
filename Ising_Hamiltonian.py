feb96f1960f416927396f941bc1a469eb51d2c7b
anastasiaangelo <145339311+anastasiaangelo@users.noreply.github.com>
2024-03-18 15:22:18 +0100

    dleting qiskitdevenv

--- Ising_Hamiltonian.py
+++ Ising_Hamiltonian.py
@@ -18,7 +18,7 @@ from pyrosetta.rosetta.core.pack.task import *
 from pyrosetta import PyMOLMover
 
 # Initiate structure, scorefunction
-pose = pyrosetta.pose_from_pdb("input_files/test1.pdb")
+pose = pyrosetta.pose_from_pdb("input_files/test.pdb")
 
 
 residue_count = pose.total_residue()
@@ -83,7 +83,7 @@ df1 = pd.DataFrame(columns=['res i', 'rot A_i', 'E_ii'])
 # pmm.apply(clone_pose)
 
 # to limit to 2 rotamers per residue
-num_rot = 2
+num_rot = 8
 
 # Loop to find Hamiltonian values Jij - interaction of rotamers on NN residues
 for residue_number in range(1, residue_count):

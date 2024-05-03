491fe0df87411d3265dab03f49429c941933c818
anastasiaangelo <145339311+anastasiaangelo@users.noreply.github.com>
2024-05-03 13:04:23 +0200

    to rettrieve results from session id

---
+++ recover_results.py
@@ -0,0 +1,253 @@
+# Point 2 of constraint studies for paper, Ising model with local penalties
+
+# Script to optimise the Hamiltonian, starting directly from the Ising Hamiltonian
+# %%
+import numpy as np
+import pandas as pd
+import time
+from copy import deepcopy
+
+num_rot = 2
+file_path = "RESULTS/sessionid/13res-2rot"
+
+########################### Configure the hamiltonian from the values calculated classically with pyrosetta ############################
+df1 = pd.read_csv("energy_files/one_body_terms.csv")
+q = df1['E_ii'].values
+num = len(q)
+N = int(num/num_rot)
+num_qubits = num
+
+print('Qii values: \n', q)
+
+df = pd.read_csv("energy_files/two_body_terms.csv")
+value = df['E_ij'].values
+Q = np.zeros((num,num))
+n = 0
+
+for i in range(0, num-2):
+    if i%2 == 0:
+        Q[i][i+2] = deepcopy(value[n])
+        Q[i+2][i] = deepcopy(value[n])
+        Q[i][i+3] = deepcopy(value[n+1])
+        Q[i+3][i] = deepcopy(value[n+1])
+        n += 2
+    elif i%2 != 0:
+        Q[i][i+1] = deepcopy(value[n])
+        Q[i+1][i] = deepcopy(value[n])
+        Q[i][i+2] = deepcopy(value[n+1])
+        Q[i+2][i] = deepcopy(value[n+1])
+        n += 2
+
+print('\nQij values: \n', Q)
+
+H = np.zeros((num,num))
+
+for i in range(num):
+    for j in range(num):
+        if i != j:
+            H[i][j] = np.multiply(0.25, Q[i][j])
+
+for i in range(num):
+    H[i][i] = -(0.5 * q[i] + sum(0.25 * Q[i][j] for j in range(num) if j != i))
+
+print('\nH: \n', H)
+
+with open(file_path, "w") as file:
+    file.write(f"H : {H} \n")
+
+# add penalty terms to the matrix so as to discourage the selection of two rotamers on the same residue - implementation of the Hammings constraint
+def add_penalty_term(M, penalty_constant, residue_pairs):
+    for i, j in residue_pairs:
+        M[i][j] += penalty_constant
+        
+    return M
+
+P = 6
+
+def generate_pairs(N):
+    pairs = [(i, i+1) for i in range(0, 2*N, 2)]
+    return pairs
+
+pairs = generate_pairs(N)
+
+M = deepcopy(H)
+M = add_penalty_term(M, P, pairs)
+
+# %% ############################################ Quantum optimisation ########################################################################
+from qiskit_algorithms.minimum_eigensolvers import QAOA
+from qiskit.quantum_info.operators import Pauli, SparsePauliOp
+from qiskit_algorithms.optimizers import COBYLA
+from qiskit.primitives import Sampler
+
+def X_op(i, num_qubits):
+    """Return an X Pauli operator on the specified qubit in a num-qubit system."""
+    op_list = ['I'] * num_qubits
+    op_list[i] = 'X'
+    return SparsePauliOp(Pauli(''.join(op_list)))
+
+def generate_pauli_zij(n, i, j):
+    if i<0 or i >= n or j<0 or j>=n:
+        raise ValueError(f"Indices out of bounds for n={n} qubits. ")
+        
+    pauli_str = ['I']*n
+
+    if i == j:
+        pauli_str[i] = 'Z'
+    else:
+        pauli_str[i] = 'Z'
+        pauli_str[j] = 'Z'
+
+    return Pauli(''.join(pauli_str))
+
+
+q_hamiltonian = SparsePauliOp(Pauli('I'*num_qubits), coeffs=[0])
+
+for i in range(num_qubits):
+    for j in range(i+1, num_qubits):
+        if M[i][j] != 0:
+            pauli = generate_pauli_zij(num_qubits, i, j)
+            op = SparsePauliOp(pauli, coeffs=[M[i][j]])
+            q_hamiltonian += op
+
+for i in range(num_qubits):
+    pauli = generate_pauli_zij(num_qubits, i, i)
+    Z_i = SparsePauliOp(pauli, coeffs=[M[i][i]])
+    q_hamiltonian += Z_i
+
+def format_sparsepauliop(op):
+    terms = []
+    labels = [pauli.to_label() for pauli in op.paulis]
+    coeffs = op.coeffs
+    for label, coeff in zip(labels, coeffs):
+        terms.append(f"{coeff:.10f} * {label}")
+    return '\n'.join(terms)
+
+print(f"\nThe hamiltonian constructed using Pauli operators is: \n", format_sparsepauliop(q_hamiltonian))
+
+#the mixer in QAOA should be a quantum operator representing transitions between configurations
+mixer_op = sum(X_op(i,num_qubits) for i in range(num_qubits))
+p = 1  # Number of QAOA layers
+initial_point = np.ones(2 * p)
+
+# %% ############################################# Hardware with QAOAAnastz ##################################################################
+from qiskit.circuit.library import QAOAAnsatz
+from qiskit_algorithms import SamplingVQE
+from qiskit_ibm_runtime import QiskitRuntimeService, Session, Sampler
+from qiskit import transpile, QuantumCircuit, QuantumRegister
+from qiskit.transpiler import CouplingMap, Layout
+
+service = QiskitRuntimeService()
+backend = service.backend("ibm_nazca")
+print('Coupling Map of hardware: ', backend.configuration().coupling_map)
+
+ansatz = QAOAAnsatz(q_hamiltonian, mixer_operator=mixer_op, reps=p)
+print('\n\nQAOAAnsatz: ', ansatz)
+
+target = backend.target
+
+# %%
+# real_coupling_map = backend.configuration().coupling_map
+# coupling_map = CouplingMap(couplinglist=real_coupling_map)
+def generate_linear_coupling_map(num_qubits):
+
+    coupling_list = [[i, i + 1] for i in range(num_qubits - 1)]
+    
+    return CouplingMap(couplinglist=coupling_list)
+
+# linear_coupling_map = generate_linear_coupling_map(num_qubits)
+# coupling_map = CouplingMap(couplinglist=[[0, 1],[0, 15], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8], [8, 9], [9, 10], [10, 11], [11, 12], [12, 13], [13, 14]])
+# coupling_map = CouplingMap(couplinglist=[[0, 1], [0, 15], [1, 2], [2, 3], [3, 4], [4, 5], [4, 16], [5, 6], [6, 7], [7, 8], [8, 9], [8, 17], [9, 10], [10, 11], [11, 12], [12, 13], [12, 18], [13, 14], [15, 19], [16, 23], [17, 27], [18, 31], [19, 20], [20, 21], [21, 22], [21, 34], [22, 23], [23, 24], [24, 25], [25, 26], [26, 27]])
+coupling_map = CouplingMap(couplinglist=[[0, 1], [0, 14], [1, 2], [3, 2], [3, 4], [4, 15], [5, 4], [6, 5], [6, 7], [7, 8], [8, 16], [9, 8], [9, 10], [10, 11], [12, 11], [13, 12], [15, 22], [17, 12], [17, 30], [18, 14], [18, 19], [20, 19], [20, 21], [20, 33], [21, 22], [23, 22], [24, 23], [25, 24]]) #nazca 26 qubits
+qr = QuantumRegister(num_qubits, 'q')
+circuit = QuantumCircuit(qr)
+trivial_layout = Layout({qr[i]: i for i in range(num_qubits)})
+ansatz_isa = transpile(ansatz, backend=backend, initial_layout=trivial_layout, coupling_map=coupling_map,
+                       optimization_level= 3, layout_method='dense', routing_method='stochastic')
+print("\n\nAnsatz layout after explicit transpilation:", ansatz_isa._layout)
+
+hamiltonian_isa = q_hamiltonian.apply_layout(ansatz_isa.layout)
+print("\n\nAnsatz layout after transpilation:", hamiltonian_isa)
+
+
+# %%
+jobs = service.jobs(session_id='crrdap27jqmg008z9m00')
+
+for job in jobs:
+    if job.status().name == 'DONE':
+        results = job.result()
+        print("Job completed successfully")
+else:
+    print("Job status:", job.status())
+
+# %%
+k = 0
+for i in range(num_qubits):
+    k += 0.5 * q[i]
+
+for i in range(num_qubits):
+    for j in range(num_qubits):
+        if i != j:
+            k += 0.5 * 0.25 * Q[i][j]
+
+def classic_energy_eval(bitstring, k):
+    spins = [1 if bit == '0' else -1 for bit in bitstring]
+    energy = 0
+
+    for i in range(num_qubits):
+        for j in range(num_qubits):
+            if i != j:
+                energy += 0.5 * H[i][j] * spins[i] * spins[j]
+
+    for i in range(num_qubits):
+        energy += H[i][i] * spins[i]
+
+    return energy + k
+
+
+def get_best_measurement_from_sampler_result(sampler_result, num_qubits):
+    if not hasattr(sampler_result, 'quasi_dists') or not isinstance(sampler_result.quasi_dists, list):
+        raise ValueError("SamplerResult does not contain 'quasi_dists' as a list")
+
+    best_bitstring = None
+    lowest_energy = float('inf')
+    highest_probability = -1
+
+    for quasi_distribution in sampler_result.quasi_dists:
+        for int_bitstring, probability in quasi_distribution.items():
+            bitstring = format(int_bitstring, '0{}b'.format(num_qubits))
+            energy = classic_energy_eval(bitstring)
+            if energy < lowest_energy:
+                lowest_energy = energy
+                best_bitstring = bitstring
+                highest_probability = probability
+
+    return best_bitstring, highest_probability, lowest_energy
+
+best_bitstring, probability, value = get_best_measurement_from_sampler_result(results, num_qubits)
+print(f"Best measurement: {best_bitstring} with ground state energy {value} and probability {probability}")
+
+# %%
+total_usage_time = 0
+for job in jobs:
+    job_result = job.usage_estimation['quantum_seconds']
+    total_usage_time += job_result
+
+print(f"Total Usage Time Hardware: {total_usage_time} seconds")
+print('\n\n')
+
+with open(file_path, "a") as file:
+    file.write("\n\nThe retrieved result of the noisy quantum optimisation using QAOAAnsatz is: \n")
+    file.write(f"Best measurement: {best_bitstring} with ground state energy {value} and probability {probability}")
+
+
+# %%
+index = ansatz_isa.layout.final_index_layout() # Maps logical qubit index to its position in bitstring
+
+measured_bitstring = best_bitstring
+original_bitstring = ['']*num_qubits
+
+for i, logical in enumerate(index):
+        original_bitstring[i] = measured_bitstring[logical]
+
+original_bitstring = ''.join(original_bitstring)
+print("Original bitstring:", original_bitstring)

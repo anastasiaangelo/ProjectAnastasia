8ec33f4ddbb2cb461be65eebb7065740b12d4f25
anastasiaangelo <145339311+anastasiaangelo@users.noreply.github.com>
2024-05-16 15:16:27 +0200

    CNOTS

--- plot_depth.py
+++ plot_depth.py
@@ -5,12 +5,29 @@ import matplotlib.pyplot as plt
 
 # Define the paths
 folder_paths = {
-    "local": "RESULTS/Depths/localpenalty-QAOA-basic",
-    "XY": "RESULTS/Depths/XY-QAOA-basic",
+    "local": "RESULTS/Depths/localpenalty-QAOA-noopt",
+    "XY": "RESULTS/Depths/XY-QAOA-noopt",
     "no_penalty": "RESULTS/nopenalty-QAOA",
-    "local_3rot": "RESULTS/Depths/3rot-localpenalty-QAOA-basic",
-    "XY_3rot": "RESULTS/Depths/3rot-XY-QAOA-basic"
+    "local_3rot": "RESULTS/Depths/3rot-localpenalty-QAOA-noopt",
+    "XY_3rot": "RESULTS/Depths/3rot-XY-QAOA-noopt"
 }
+
+# folder_paths = {
+#     "local": "RESULTS/Depths/localpenalty-QAOA-basic",
+#     "XY": "RESULTS/Depths/XY-QAOA-basic",
+#     "no_penalty": "RESULTS/nopenalty-QAOA",
+#     "local_3rot": "RESULTS/Depths/3rot-localpenalty-QAOA-basic",
+#     "XY_3rot": "RESULTS/Depths/3rot-XY-QAOA-basic"
+# }
+
+# folder_paths = {
+#     "local": "RESULTS/Depths/localpenalty-QAOA",
+#     "XY": "RESULTS/Depths/XY-QAOA",
+#     "no_penalty": "RESULTS/nopenalty-QAOA",
+#     "local_3rot": "RESULTS/Depths/3rot-localpenalty-QAOA",
+#     "XY_3rot": "RESULTS/Depths/3rot-XY-QAOA"
+# }
+
 
 def custom_sort_key(filename):
     numbers = re.findall(r'\d+', filename)
@@ -38,19 +55,18 @@ num_qubits_3 = []
 # Calculate the ratios
 for df_local, df_XY, df_no_penalty in zip(data_local, data_XY, data_no_penalty):
     num_qubits = df_no_penalty['Number of qubits']  # From no_penalty file
-    depth_local = df_local['Depth of the circuit']
-    depth_XY = df_XY['Depth of the circuit']
+    depth_local = df_local['CNOTs']
+    depth_XY = df_XY['CNOTs']
     
     depths.append((depth_local, depth_XY))
     num_qubits_list.append(num_qubits)
 
 for df_local_3rot, df_XY_3rot in zip(data_3rot_local, data_3rot_XY):
-    depth_local_3rot = df_local_3rot['Depth of the circuit']
-    depth_XY_3rot = df_XY_3rot['Depth of the circuit']
+    depth_local_3rot = df_local_3rot['CNOTs']
+    depth_XY_3rot = df_XY_3rot['CNOTs']
     
     depths_3.append((depth_local_3rot, depth_XY_3rot))
 
-num_qubits_list = [8, 10, 12, 14, 16, 18, 20, 22, 24, 26]
 num_qubits_3 = [12, 15, 18, 21, 24, 27, 30]
 print(num_qubits_list)
 print(num_qubits_3)
@@ -72,6 +88,6 @@ ax.plot(x3, y_XY_3rot, label="XY mixer 3 rot", marker='1')
 
 ax.set_xlabel("Number of Qubits")
 ax.set_ylabel("Number of CNOTs")
-ax.set_title("Number of CNOTs by Number of Qubits Basic Routing")
+ax.set_title("Number of CNOTs by Number of Qubits Basic Routing and 3 level optimisation")
 ax.legend()
 plt.show()

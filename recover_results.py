cb885ca62e1a45d179b933e6f7f03fad7232d500
anastasiaangelo <145339311+anastasiaangelo@users.noreply.github.com>
2024-05-21 14:48:19 +0200

    corrected coupling maps

--- recover_results.py
+++ recover_results.py
@@ -6,8 +6,8 @@ import numpy as np
 import pandas as pd
 from copy import deepcopy
 
-num_rot = 2
-file_path = "RESULTS/sessionid/15res-2rot.csv"
+num_rot = 3
+file_path = "RESULTS/sessionid/8res-3rot.csv"
 
 ########################### Configure the hamiltonian from the values calculated classically with pyrosetta ############################
 df1 = pd.read_csv("energy_files/one_body_terms.csv")
@@ -23,19 +23,31 @@ value = df2['E_ij'].values
 Q = np.zeros((num,num))
 n = 0
 
-for i in range(0, num-2):
-    if i%2 == 0:
-        Q[i][i+2] = deepcopy(value[n])
-        Q[i+2][i] = deepcopy(value[n])
-        Q[i][i+3] = deepcopy(value[n+1])
-        Q[i+3][i] = deepcopy(value[n+1])
-        n += 2
-    elif i%2 != 0:
-        Q[i][i+1] = deepcopy(value[n])
-        Q[i+1][i] = deepcopy(value[n])
-        Q[i][i+2] = deepcopy(value[n+1])
-        Q[i+2][i] = deepcopy(value[n+1])
-        n += 2
+# 2 rot per residue
+# for i in range(0, num-2):
+#     if i%2 == 0:
+#         Q[i][i+2] = deepcopy(value[n])
+#         Q[i+2][i] = deepcopy(value[n])
+#         Q[i][i+3] = deepcopy(value[n+1])
+#         Q[i+3][i] = deepcopy(value[n+1])
+#         n += 2
+#     elif i%2 != 0:
+#         Q[i][i+1] = deepcopy(value[n])
+#         Q[i+1][i] = deepcopy(value[n])
+#         Q[i][i+2] = deepcopy(value[n+1])
+#         Q[i+2][i] = deepcopy(value[n+1])
+#         n += 2
+
+# 3 rot per residue
+for j in range(0, num-3, num_rot):
+    for i in range(j, j+num_rot):
+        Q[i][j+3] = deepcopy(value[n])
+        Q[j+3][i] = deepcopy(value[n])
+        Q[i][j+4] = deepcopy(value[n+1])
+        Q[j+4][i] = deepcopy(value[n+1])
+        Q[i][j+5] = deepcopy(value[n+2])
+        Q[j+5][i] = deepcopy(value[n+2])
+        n += num_rot
 
 print('\nQij values: \n', Q)
 
@@ -51,29 +63,36 @@ for i in range(num):
 
 print('\nH: \n', H)
 
-with open(file_path, "w") as file:
-    file.write(f"H : {H} \n")
+k = 0
+for i in range(num_qubits):
+    k += 0.5 * q[i]
 
-# add penalty terms to the matrix so as to discourage the selection of two rotamers on the same residue - implementation of the Hammings constraint
-def add_penalty_term(M, penalty_constant, residue_pairs):
-    for i, j in residue_pairs:
-        M[i][j] += penalty_constant
+for i in range(num_qubits):
+    for j in range(num_qubits):
+        if i != j:
+            k += 0.5 * 0.25 * Q[i][j]
+
+# # add penalty terms to the matrix so as to discourage the selection of two rotamers on the same residue - implementation of the Hammings constraint
+# def add_penalty_term(M, penalty_constant, residue_pairs):
+#     for i, j in residue_pairs:
+#         M[i][j] += penalty_constant
         
-    return M
+#     return M
 
-P = 6
+# P = 6
 
-def generate_pairs(N):
-    pairs = [(i, i+1) for i in range(0, 2*N, 2)]
-    return pairs
+# def generate_pairs(N):
+#     pairs = [(i, i+1) for i in range(0, 2*N, 2)]
+#     return pairs
 
-pairs = generate_pairs(N)
+# pairs = generate_pairs(N)
 
-M = deepcopy(H)
-M = add_penalty_term(M, P, pairs)
+# M = deepcopy(H)
+# M = add_penalty_term(M, P, pairs)
 
 # %% ############################################ Quantum optimisation ########################################################################
 from qiskit.quantum_info.operators import Pauli, SparsePauliOp
+from qiskit import QuantumCircuit
 
 def X_op(i, num_qubits):
     """Return an X Pauli operator on the specified qubit in a num-qubit system."""
@@ -100,14 +119,14 @@ q_hamiltonian = SparsePauliOp(Pauli('I'*num_qubits), coeffs=[0])
 
 for i in range(num_qubits):
     for j in range(i+1, num_qubits):
-        if M[i][j] != 0:
+        if H[i][j] != 0:
             pauli = generate_pauli_zij(num_qubits, i, j)
-            op = SparsePauliOp(pauli, coeffs=[M[i][j]])
+            op = SparsePauliOp(pauli, coeffs=[H[i][j]])
             q_hamiltonian += op
 
 for i in range(num_qubits):
     pauli = generate_pauli_zij(num_qubits, i, i)
-    Z_i = SparsePauliOp(pauli, coeffs=[M[i][i]])
+    Z_i = SparsePauliOp(pauli, coeffs=[H[i][i]])
     q_hamiltonian += Z_i
 
 def format_sparsepauliop(op):
@@ -121,9 +140,55 @@ def format_sparsepauliop(op):
 print(f"\nThe hamiltonian constructed using Pauli operators is: \n", format_sparsepauliop(q_hamiltonian))
 
 #the mixer in QAOA should be a quantum operator representing transitions between configurations
-mixer_op = sum(X_op(i,num_qubits) for i in range(num_qubits))
 p = 1  # Number of QAOA layers
 initial_point = np.ones(2 * p)
+
+def create_custom_xy_mixer(num_qubits):
+    hamiltonian = SparsePauliOp(Pauli('I' * num_qubits), coeffs=[0])
+    for i in range(0, num_qubits - 2, 3): 
+        x1x2 = ['I'] * num_qubits
+        y1y2 = ['I'] * num_qubits
+        x2x3 = ['I'] * num_qubits
+        y2y3 = ['I'] * num_qubits
+        x1x3 = ['I'] * num_qubits
+        y1y3 = ['I'] * num_qubits
+
+        x1x2[i] = 'X'
+        x1x2[i+1] = 'X'
+        y1y2[i] = 'Y'
+        y1y2[i+1] = 'Y'
+        x2x3[i+1] = 'X'
+        x2x3[i+2] = 'X'
+        y2y3[i+1] = 'Y'
+        y2y3[i+2] = 'Y'
+        x1x3[i] = 'X'
+        x1x3[i+2] = 'X'
+        y1y3[i] = 'Y'
+        y1y3[i+2] = 'Y' 
+
+        x1x2 = SparsePauliOp(Pauli(''.join(x1x2)), coeffs=[1/2])
+        y1y2 = SparsePauliOp(Pauli(''.join(y1y2)), coeffs=[1/2])
+        x2x3 = SparsePauliOp(Pauli(''.join(x2x3)), coeffs=[1/2])
+        y2y3 = SparsePauliOp(Pauli(''.join(y2y3)), coeffs=[1/2])
+        x1x3 = SparsePauliOp(Pauli(''.join(x1x3)), coeffs=[1/2])
+        y1y3 = SparsePauliOp(Pauli(''.join(y1y3)), coeffs=[1/2])
+
+        hamiltonian += x1x2 + y1y2 + x2x3 + y2y3 + x1x3 + y1y3
+    return hamiltonian
+
+XY_mixer = create_custom_xy_mixer(num_qubits)
+
+def generate_initial_bitstring(num_qubits):
+    pattern = '100'
+    bitstring = (pattern * (num_qubits // 3 + 1))[:num_qubits]
+    return bitstring
+
+initial_bitstring = generate_initial_bitstring(num_qubits)
+state_vector = np.zeros(2**num_qubits)
+indexx = int(initial_bitstring, 2)
+state_vector[indexx] = 1
+qc = QuantumCircuit(num_qubits)
+qc.initialize(state_vector, range(num_qubits))
 
 # %% ############################################# Hardware with QAOAAnastz ##################################################################
 from qiskit.circuit.library import QAOAAnsatz
@@ -135,38 +200,39 @@ service = QiskitRuntimeService()
 backend = service.backend("ibm_torino")
 print('Coupling Map of hardware: ', backend.configuration().coupling_map)
 
-ansatz = QAOAAnsatz(q_hamiltonian, mixer_operator=mixer_op, reps=p)
+ansatz = QAOAAnsatz(q_hamiltonian, mixer_operator=XY_mixer, reps=p)
 print('\n\nQAOAAnsatz: ', ansatz)
+
+opt_parameters = [3.15625, 1.0]
+ansatz_one_rep = QAOAAnsatz(q_hamiltonian, mixer_operator=XY_mixer, reps=1)
+params_dict = {param: value for param, value in zip(ansatz_one_rep.parameters, opt_parameters)}
+bound_circuit = ansatz_one_rep.assign_parameters(params_dict)
 
 target = backend.target
 
 # %%
-# real_coupling_map = backend.configuration().coupling_map
-# coupling_map = CouplingMap(couplinglist=real_coupling_map)
-def generate_linear_coupling_map(num_qubits):
+filtered_coupling_map = [coupling for coupling in backend.configuration().coupling_map if coupling[0] < num_qubits and coupling[1] < num_qubits]
 
-    coupling_list = [[i, i + 1] for i in range(num_qubits - 1)]
-    
-    return CouplingMap(couplinglist=coupling_list)
-
-# linear_coupling_map = generate_linear_coupling_map(num_qubits)
-# coupling_map = CouplingMap(couplinglist=[[0, 1],[0, 15], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8], [8, 9], [9, 10], [10, 11], [11, 12], [12, 13], [13, 14]])
-# coupling_map = CouplingMap(couplinglist=[[0, 1], [0, 15], [1, 2], [2, 3], [3, 4], [4, 5], [4, 16], [5, 6], [6, 7], [7, 8], [8, 9], [8, 17], [9, 10], [10, 11], [11, 12], [12, 13], [12, 18], [13, 14], [15, 19], [16, 23], [17, 27], [18, 31], [19, 20], [20, 21], [21, 22], [21, 34], [22, 23], [23, 24], [24, 25], [25, 26], [26, 27]])
-coupling_map = CouplingMap(couplinglist=[[0, 1], [0, 15], [1, 0], [1, 2], [2, 1], [2, 3], [3, 2], [3, 4], [4, 3], [4, 5], [4, 16], [5, 4], [5, 6], [6, 5], [6, 7], [7, 6], [7, 8], [8, 7], [8, 9], [8, 17], [9, 8], [9, 10], [10, 9], [10, 11], [11, 10], [11, 12], [12, 11], [12, 13], [12, 18], [13, 12], [13, 14], [14, 13], [15, 0], [15, 19], [16, 4], [16, 23], [17, 8], [17, 27], [18, 12], [18, 31], [19, 15], [19, 20], [20, 19], [20, 21], [21, 20], [21, 22], [21, 34], [22, 21], [22, 23], [23, 16], [23, 22], [23, 24], [24, 23], [24, 25], [25, 24], [25, 26], [25, 35], [26, 25], [26, 27], [27, 17], [27, 26], [27, 28], [28, 27], [28, 29], [29, 28]])
-# coupling_map = CouplingMap(couplinglist=[[0, 1], [0, 14], [1, 2], [3, 2], [3, 4], [4, 15], [5, 4], [6, 5], [6, 7], [7, 8], [8, 16], [9, 8], [9, 10], [10, 11], [12, 11], [13, 12], [15, 22], [17, 12], [17, 30], [18, 14], [18, 19], [20, 19], [20, 21], [20, 33], [21, 22], [23, 22], [24, 23], [25, 24]]) #nazca 26 qubits
 qr = QuantumRegister(num_qubits, 'q')
 circuit = QuantumCircuit(qr)
 trivial_layout = Layout({qr[i]: i for i in range(num_qubits)})
-ansatz_isa = transpile(ansatz, backend=backend, initial_layout=trivial_layout, coupling_map=coupling_map,
-                       optimization_level= 3, layout_method='dense', routing_method='stochastic')
-print("\n\nAnsatz layout after explicit transpilation:", ansatz_isa._layout)
 
-hamiltonian_isa = q_hamiltonian.apply_layout(ansatz_isa.layout)
-print("\n\nAnsatz layout after transpilation:", hamiltonian_isa)
+# ansatz_isa = transpile(ansatz, backend=backend, initial_layout=trivial_layout, coupling_map=filtered_coupling_map,
+#                        optimization_level= 3, layout_method='dense', routing_method='stochastic')
+# print("\n\nAnsatz layout after explicit transpilation:", ansatz_isa._layout)
+
+# hamiltonian_isa = q_hamiltonian.apply_layout(ansatz_isa.layout)
+# print("\n\nAnsatz layout after transpilation:", hamiltonian_isa)
+
+ansatz_one_rep_isa = transpile(ansatz_one_rep, backend=backend, initial_layout=trivial_layout, coupling_map=filtered_coupling_map,
+                               optimization_level=3, layout_method='trivial', routing_method='stochastic')
+hamiltonian_isa_one_rep = q_hamiltonian.apply_layout(ansatz_one_rep_isa.layout)
+print("\n\nAnsatz layout after transpilation:", hamiltonian_isa_one_rep)
+
 
 
 # %%
-jobs = service.jobs(session_id='crsn8xvx484g008f4200')
+jobs = service.jobs(session_id='cs3kwm275q40008ttagg')
 
 for job in jobs:
     if job.status().name == 'DONE':
@@ -179,19 +245,29 @@ else:
 from qiskit_aer.primitives import Estimator
 from qiskit import QuantumCircuit, transpile
 from qiskit_aer import Aer
-k = 0
-for i in range(num_qubits):
-    k += 0.5 * q[i]
 
-for i in range(num_qubits):
-    for j in range(num_qubits):
-        if i != j:
-            k += 0.5 * 0.25 * Q[i][j]
+def int_to_bitstring(state, total_bits):
+    """Converts an integer state to a binary bitstring with padding of leading zeros."""
+    return format(state, '0{}b'.format(total_bits))
 
 def check_hamming(bitstring, substring_size):
     """Check if each substring contains exactly one '1'."""
     substrings = [bitstring[i:i+substring_size] for i in range(0, len(bitstring), substring_size)]
     return all(sub.count('1') == 1 for sub in substrings)
+
+def extend_hamiltonian_with_ancillas(hamiltonian, num_ancillas):
+    """Extend the Hamiltonian to include ancilla qubits with zero coefficients."""
+    num_logical_qubits = hamiltonian.num_qubits
+    total_qubits = num_logical_qubits + num_ancillas
+    identity_term = SparsePauliOp(Pauli('I' * total_qubits), coeffs=[0])
+    
+    for pauli, coeff in zip(hamiltonian.paulis, hamiltonian.coeffs):
+        extended_pauli = Pauli(pauli.to_label() + 'I' * num_ancillas)
+        term = SparsePauliOp(extended_pauli, coeffs=[coeff])
+        identity_term += term
+        
+    return identity_term
+
 
 def calculate_bitstring_energy(bitstring, hamiltonian, backend=None):
     """
@@ -206,8 +282,9 @@ def calculate_bitstring_energy(bitstring, hamiltonian, backend=None):
         float: The calculated energy of the bitstring.
     """
     # Prepare the quantum circuit for the bitstring
-    num_qubits = len(bitstring)
-    qc = QuantumCircuit(num_qubits)
+    num_qubits_qc = ansatz_one_rep_isa.num_qubits
+    print('qubits in cirucit ', num_qubits_qc)
+    qc = QuantumCircuit(num_qubits_qc)
     for i, char in enumerate(bitstring):
         if char == '1':
             qc.x(i)  # Apply X gate if the bit in the bitstring is 1
@@ -216,36 +293,48 @@ def calculate_bitstring_energy(bitstring, hamiltonian, backend=None):
     if backend is None:
         backend = Aer.get_backend('aer_simulator_statevector')
 
-    qc = transpile(qc, backend, coupling_map=coupling_map)
+    qc = transpile(qc, backend=backend, coupling_map=filtered_coupling_map)
     estimator = Estimator()
-    resultt = estimator.run(observables=[hamiltonian], circuits=[qc], backend=backend).result()
+    extended_hamiltonian = extend_hamiltonian_with_ancillas(hamiltonian, num_ancillas)
+    print('extended ham ', extended_hamiltonian)
+    resultt = estimator.run(observables=[extended_hamiltonian], circuits=[qc], backend=backend).result()
 
     return resultt.values[0].real
 
-def get_best_measurement_from_sampler_result(sampler_result, num_qubits):
+def get_best_measurement_from_sampler_result(sampler_result, num_qubits, num_ancillas):
+    logical_qubits = num_qubits - num_ancillas
+    print('logical qubits', logical_qubits)
     if not hasattr(sampler_result, 'quasi_dists') or not isinstance(sampler_result.quasi_dists, list):
         raise ValueError("SamplerResult does not contain 'quasi_dists' as a list")
 
     best_bitstring = None
     lowest_energy = float('inf')
     highest_probability = -1
+    total_bitstrings = 0
+    valid_bitstrings = 0
 
     for quasi_distribution in sampler_result.quasi_dists:
-        for int_bitstring, probability in quasi_distribution.items():
-            bitstring = format(int_bitstring, '0{}b'.format(num_qubits))
-            if check_hamming(bitstring, num_rot):
-                energy = calculate_bitstring_energy(bitstring, q_hamiltonian, backend)
-                print(f"Bitstring: {bitstring}, Energy: {energy}, Probability: {probability}")
+        for state, probability in quasi_distribution.items():
+            bitstring = int_to_bitstring(state, num_qubits)
+            logical_bitstring = bitstring[:logical_qubits]
+            print('logical bitstring', logical_bitstring)
+            total_bitstrings += 1
+            if check_hamming(logical_bitstring, num_rot):
+                energy = calculate_bitstring_energy(logical_bitstring, q_hamiltonian, backend)
+                print(f"Bitstring: {logical_bitstring}, Energy: {energy}, Probability: {probability}")
+                valid_bitstrings += 1
                 if energy < lowest_energy:
                     lowest_energy = energy
-                    best_bitstring = bitstring
+                    best_bitstring = logical_bitstring
                     highest_probability = probability
 
-    return best_bitstring, highest_probability, lowest_energy
+    return best_bitstring, highest_probability, lowest_energy, total_bitstrings, valid_bitstrings
 
-best_bitstring, probability, value = get_best_measurement_from_sampler_result(results, num_qubits)
-print(f"Best measurement: {best_bitstring} with ground state energy {value+N*P+k} and probability {probability}")
-
+num_ancillas = ansatz_one_rep_isa.num_qubits - num_qubits
+best_bitstring, probability, value, total_bitstrings, valid_bitstrings = get_best_measurement_from_sampler_result(results, ansatz_one_rep_isa.num_qubits, num_ancillas)
+fraction_satisfying_hamming = valid_bitstrings / total_bitstrings
+print(f"Best measurement: {best_bitstring} with ground state energy {value+k} and probability {probability}")
+print(f"Fraction of bitstrings that satisfy the Hamming constraint: {fraction_satisfying_hamming}")
 # %%
 total_usage_time = 0
 for job in jobs:
@@ -257,10 +346,11 @@ print('\n\n')
 
 data = {
         "Experiment": ["Hardware simulation QAOAAnsatz"],
-        "Ground State Energy": [value+N*P+k],
+        "Ground State Energy": [value+k],
         "Best Measurement": [best_bitstring],
         "Execution Time (seconds)": [total_usage_time],
-        "Number of qubits": [num_qubits]
+        "Number of qubits": [num_qubits], 
+        "Fraction of bitstrings that satisfy the Hamming constraint": [fraction_satisfying_hamming]
 }
 df = pd.DataFrame(data)
 df.to_csv(file_path, index=False)
